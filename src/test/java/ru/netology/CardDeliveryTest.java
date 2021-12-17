aaade40518574e7ad882767cea67b44a34868a09
Olga Stashenyuk <olgastachenyk@mail.ru>
2021-12-17 18:58:45 +0300

    Correct test

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,13 +1,10 @@
 package ru.netology;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
@@ -16,15 +13,13 @@ public class CardDeliveryTest {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Киров");
         $(".calendar-input__custom-control").click();
-        $$("[data-day]").get(1).click();
+        $$("[data-day]").get(2).click();
         $("[data-test-id='name'] input").setValue("Петров Петр");
         $("[data-test-id='phone'] input").setValue("+79875674356");
         $(".checkbox__box").click();
         $("[type='button'] [class=button__text]").click();
-//        $("[data-test-id='notification']").shouldBe(appear);
-        $("[class='notification__title']").shouldHave(exactText("Успешно!"));
+        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(20)).shouldHave(text("Успешно"));
         $("[class='notification__content']").shouldHave(text("Встреча успешно забронирована на " + $("[data-test-id=date] input").getValue()));
-//        $(withText("Успешно!")).shouldBe(Condition.appear, Duration.ofSeconds(5));
-//        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на"));
+
     }
 }
